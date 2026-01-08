13706458ef4baa6f639bd9148137d80d7bb60fcb
Peter Decuyper <peter.decuyper@district09.gent>
2026-01-08 15:42:51 +0100

    VG-2746: Update district09/qa-php from 1.0 to 2.2

--- src/Value/Geometry/GeometryInterface.php
+++ src/Value/Geometry/GeometryInterface.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace District09\Gent\Lez\Value\Geometry;
 
 use DigipolisGent\Value\CollectionInterface;
-use DigipolisGent\Value\ValueInterface;
 
 /**
  * A geometrical object.
